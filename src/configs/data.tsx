f7c84d68d73efe91439c2fe4e66085e249d0f0c3
advhelio <48299713+advhelio@users.noreply.github.com>
2025-07-19 12:46:15 -0700

    Update data.tsx

--- src/configs/data.tsx
+++ src/configs/data.tsx
@@ -3,7 +3,7 @@ import { Header, Social, Galleries } from "../types";
 export const header: Header = {
   title: "Hello, I'm Olga Panasenco.",
   description: "Welcome to my art gallery!",
-  image: require("./Olga_portret2.png"),
+  image: require("./Olga_portret_2.png"),
   disabled: false,
 };
 
