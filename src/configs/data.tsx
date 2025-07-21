fc67d8a8f395a0c8514304d6ade5e35a6cbbd523
advhelio <48299713+advhelio@users.noreply.github.com>
2025-07-21 17:15:42 -0600

    Fixing data.tsx 4

--- src/configs/data.tsx
+++ src/configs/data.tsx
@@ -3,21 +3,27 @@ import { Header, Social, Galleries } from "../types";
 export const header: Header = {
   title: "Hello, I'm Olga.",
   description: "Welcome to my art gallery!",
-  image: require("images/Olga_portret_2.png"),
+  image: require("./images/Olga_portret_2.png"),
   disabled: false,
 };
 
+export const social: Social = {
+  github: "https://github.com/",
+  twitter: "https://twitter.com/",
+  linkedin: "https://www.linkedin.com/in/",
+  disabled: true,
+};
 
 export const galleries: Galleries = [
   {
     category: "Paintings",
     images: [
       {
-        name: "images/Green_cow.jpg",
+        name: "Green_cow.jpg",
         description: "Description of image Green_cow",
       },
       {
-        name: "images/painting_1.jpg",
+        name: "painting_1.jpg",
         description: "Description of image painting_1",
       },
     ],
@@ -26,7 +32,7 @@ export const galleries: Galleries = [
     category: "Art Prints/Cyanotype",
     images: [
       {
-        name: "images/cyan_ink_1.jpg",
+        name: "cyan_ink_1.jpg",
         description: "Description of image 1",
       },
     ],
